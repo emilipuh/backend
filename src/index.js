fc9ccf98f00a6821cbd08ae5e57f2fe34792dc0d
Emili Puh <epuh@unipu.hr>
2024-02-12 15:41:57 +0100

    Manje izmjene u store.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import express from "express";
+import cors from "cors";
+import data from "./store.js";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(cors());
+
+app.get("/", (req, res) => {
+    let stanjeRacuna = data.stanje;
+    res.json(stanjeRacuna);
+});
+
+app.get("/pregledPrihoda", (req, res) => {
+    let prihodi = data.prihodi.data;
+    res.json(prihodi);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
