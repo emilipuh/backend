00ffe7a0b81609c818521ac8446a6056d73af391
Emili Puh <epuh@unipu.hr>
2024-02-13 15:10:25 +0100

    Dodana POST metoda za testiranje backenda

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 import express from "express";
 import cors from "cors";
-import data from "./store.js";
+import podaci from "./store.js";
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    let stanjeRacuna = data.stanje;
+    let stanjeRacuna = podaci.stanje;
     res.json(stanjeRacuna);
 });
 
 app.get("/pregledPrihoda", (req, res) => {
-    let prihodi = data.prihodi.data;
+    let prihodi = podaci.prihodi;
     res.json(prihodi);
+});
+
+app.post("/dodajPrihod", (req, res) => {
+    // dodajemo novi prihod
+    let prihod = req.body;
+    console.log(req.body)
+    podaci.prihodi.push(prihod); // jako neuredno
+    res.json("OK");
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
