7d83b6d4ba398673441b30eb36a3e482949d43e3
Emili Puh <epuh@unipu.hr>
2024-02-15 13:09:28 +0100

    Dodan modul za povezivanje na mongodb

---
+++ src/db.js
@@ -0,0 +1,30 @@
+const { MongoClient, ServerApiVersion, Db } = require("mongodb");
+const uri =
+  "mongodb+srv://epuh:epuh@cluster0.laryrkw.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let db = null;
+
+export default async () => {
+  if (db && client.isConnected) {
+    console.log("Već spojeno");
+    return db;
+  } else {
+    try {
+      await client.connect();
+      db = client.db("expense_track");
+      console.log("Uspješno povezivanje na bazu podataka!");
+      return db;
+    } catch (error) {
+      console.error("Greška prilikom povezivanja na bazu podataka:", error);
+      throw error; // Odbacuje grešku kako bi se obradila na višoj razini
+    }
+  }
+};
