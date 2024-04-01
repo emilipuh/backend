853ca130a6c8007103c30f7fc8e8e17a5ad5de60
Emili Puh <epuh@unipu.hr>
2024-04-01 09:44:49 +0200

    Manje izmjene u kodu

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import auth from "./auth";
 import { BSON } from "mongodb";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
